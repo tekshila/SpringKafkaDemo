e264d475492acbc5e678b41981f35712d453a19f
tekshila <67528077+tekshila@users.noreply.github.com>
2021-03-04 15:23:12 -0500

    First version of commitin the overall project and consumers

---
+++ spring-kafka-producer-demo/src/main/java/com/tekshila/simplekafkaproducer/SpringKafkaProducerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tekshila.simplekafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaProducerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringKafkaProducerDemoApplication.class, args);
+    }
+
+}
